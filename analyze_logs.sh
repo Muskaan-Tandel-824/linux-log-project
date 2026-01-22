ab9923bd45cfad1b09b1f047ee5c0c7c27910e04
muskan tandel <muskantandel824@gmail.com>
2026-01-22 14:02:49 +0000

    added new attributes

--- analyze_logs.sh
+++ analyze_logs.sh
@@ -1,24 +1,20 @@
-## analyze_logs.sh
-
-```bash
 #!/bin/bash
 
 LOG_FILE="app.log"
 REPORT_FILE="error_report.txt"
 
-# Check if log file exists
+#check if log file exists 
 if [ ! -f "$LOG_FILE" ]; then
-  echo "Log file not found!"
-  exit 1
+	echo "Log file not found ! "
+	exit 1
 fi
 
-# Count errors
+#count errors
 ERROR_COUNT=$(grep -c "ERROR" "$LOG_FILE")
 
-# Generate error report
-grep "ERROR" "$LOG_FILE" > "$REPORT_FILE"
+#Generate error report
+grep "ERROR" $LOG_FILE >> "$REPORT_FILE"
 
-echo "Log Analysis Completed"
-echo "Total Errors Found: $ERROR_COUNT"
-echo "Error report generated in $REPORT_FILE"
+echo "Log analysis Completed"
+echo "Total Errors found: $ERROR_COUNT"
 
